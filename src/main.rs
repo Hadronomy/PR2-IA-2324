bf43f4148df613a10306f9fb912e3dcf34d00b4d
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-10-13 17:48:55 +0100

    feat: add ability to regenerate the maze

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
 use bevy::math::*;
-use bevy::{prelude::*, diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin}, window::PrimaryWindow};
-use bevy_ecs_tilemap::{prelude::*, helpers::square_grid::neighbors::*};
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
+use bevy_ecs_tilemap::{helpers::square_grid::neighbors::*, prelude::*};
+use rand::Rng;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins.set(WindowPlugin{
-            primary_window: Some(Window {
-                title: String::from(
-                    "Basic Example - Press Space to change Texture and H to show/hide tilemap.",
-                ),
-                ..Default::default()
-            }),
-            ..default()
-        }).set(ImagePlugin::default_nearest()), 
+        .add_plugins((
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("A*"),
+                        ..Default::default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
             // LogDiagnosticsPlugin::default(),
-            FrameTimeDiagnosticsPlugin::default()
+            FrameTimeDiagnosticsPlugin::default(),
         ))
         .init_resource::<CursorPos>()
+        .add_event::<GenerateMazeEvent>()
         .add_plugins(TilemapPlugin)
-        .add_systems(Startup, startup.in_set(ConfigureMaze))
-        .add_systems(Update, (update_cursor_pos, text_update_system))
+        .add_systems(Startup, startup)
+        .add_systems(Startup, init_maze.in_set(ConfigureMaze))
+        .add_systems(
+            Update,
+            (
+                update_cursor_pos,
+                text_update_system,
+                generate_maze.in_set(ConfigureMaze),
+                regenerate_on_click.after(ConfigureMaze),
+            ),
+        )
         .run();
 }
 
@@ -28,6 +43,12 @@ pub struct ConfigureMaze;
 
 #[derive(Component)]
 struct Maze;
+
+#[derive(Event)]
+struct GenerateMazeEvent;
+
+#[derive(Resource)]
+struct ActiveMaze(Entity);
 
 #[derive(Component)]
 struct FpsText;
@@ -38,53 +59,11 @@ struct CursorPosText;
 #[derive(Component)]
 enum TileType {
     Wall,
-    Floor
+    Floor,
 }
 
-fn startup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    #[cfg(all(not(feature = "atlas"), feature = "render"))] array_texture_loader: Res<
-        ArrayTextureLoader,
-    >,
-) {
+fn startup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-
-    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
-
-    let map_size = TilemapSize { x: 64, y: 32 };
-
-    // Create a tilemap entity a little early.
-    // We want this entity early because we need to tell each tile which tilemap entity
-    // it is associated with. This is done with the TilemapId component on each tile.
-    // Eventually, we will insert the `TilemapBundle` bundle on the entity, which
-    // will contain various necessary components, such as `TileStorage`.
-    let tilemap_entity = commands.spawn_empty().id();
-
-    // To begin creating the map we will need a `TileStorage` component.
-    // This component is a grid of tile entities and is used to help keep track of individual
-    // tiles in the world. If you have multiple layers of tiles you would have a tilemap entity
-    // per layer, each with their own `TileStorage` component.
-    let mut tile_storage = TileStorage::empty(map_size);
-
-    // Spawn the elements of the tilemap.
-    // Alternatively, you can use helpers::filling::fill_tilemap.
-    for x in 0..map_size.x {
-        for y in 0..map_size.y {
-            let tile_pos = TilePos { x, y };
-            let tile_entity = commands
-                .spawn(TileBundle {
-                    position: tile_pos,
-                    tilemap_id: TilemapId(tilemap_entity),
-                    texture_index: TileTextureIndex(5),
-                    color: TileColor(Color::BLACK),
-                    ..Default::default()
-                })
-                .id();
-            commands.entity(tile_entity).insert(TileType::Wall);
-            tile_storage.set(&tile_pos, tile_entity);
-        }
-    }
 
     // let neighbor_positions =
     //      Neighbors::get_square_neighboring_positions(&TilePos { x: 0, y: 0 }, &map_size, true);
@@ -94,35 +73,7 @@ fn startup(
     // assert!(tile_storage.get(&TilePos { x: 0, y: 0 }).is_some());
     // assert_eq!(neighbor_entities.iter().count(), 3); // Only 3 neighbors since negative is outside of map.
 
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
-    let grid_size = tile_size.into();
-    let map_type = TilemapType::default();
-
     // This changes some of our tiles by looking at neighbors.
-    for x in 0..map_size.x {
-        for y in 0..map_size.y {
-            let is_wall: bool = rand::random();
-            // Grabbing neighbors is easy.
-            if is_wall {
-                commands
-                    .entity(tile_storage.get(&TilePos { x, y }).unwrap())
-                    .insert(TileColor(Color::WHITE))
-                    .insert(TileType::Wall);
-            }
-            
-        }
-    }
-
-    commands.entity(tilemap_entity).insert(TilemapBundle {
-        grid_size,
-        map_type,
-        size: map_size,
-        storage: tile_storage,
-        texture: TilemapTexture::Single(texture_handle),
-        tile_size,
-        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
-        ..Default::default()
-    }).insert(Maze);
 
     commands.spawn((
         // Create a TextBundle that has a Text with a list of sections.
@@ -173,7 +124,6 @@ fn startup(
                 // If no font is specified, it will use the default font.
                 ..default()
             }),
-            
         ]),
         CursorPosText,
     ));
@@ -190,11 +140,148 @@ fn startup(
     }
 }
 
+fn init_maze(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut ev_generate_maze: EventWriter<GenerateMazeEvent>,
+    #[cfg(all(not(feature = "atlas"), feature = "render"))] array_texture_loader: Res<
+        ArrayTextureLoader,
+    >,
+) {
+    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
+
+    let map_size = TilemapSize { x: 64, y: 32 };
+
+    // Create a tilemap entity a little early.
+    // We want this entity early because we need to tell each tile which tilemap entity
+    // it is associated with. This is done with the TilemapId component on each tile.
+    // Eventually, we will insert the `TilemapBundle` bundle on the entity, which
+    // will contain various necessary components, such as `TileStorage`.
+    let tilemap_entity = commands.spawn_empty().id();
+
+    // To begin creating the map we will need a `TileStorage` component.
+    // This component is a grid of tile entities and is used to help keep track of individual
+    // tiles in the world. If you have multiple layers of tiles you would have a tilemap entity
+    // per layer, each with their own `TileStorage` component.
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    // Spawn the elements of the tilemap.
+    // Alternatively, you can use helpers::filling::fill_tilemap.
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    texture_index: TileTextureIndex(5),
+                    color: TileColor(Color::BLACK),
+                    ..Default::default()
+                })
+                .insert(TileType::Wall)
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    commands
+        .entity(tilemap_entity)
+        .insert(Maze)
+        .insert(TilemapBundle {
+            grid_size,
+            map_type,
+            size: map_size,
+            storage: tile_storage,
+            texture: TilemapTexture::Single(texture_handle),
+            tile_size,
+            transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+            ..Default::default()
+        });
+
+    commands.insert_resource(ActiveMaze(tilemap_entity));
+    ev_generate_maze.send(GenerateMazeEvent {});
+}
+
+fn generate_maze(
+    mut commands: Commands,
+    storage_query: Query<&TileStorage>,
+    active_maze: Res<ActiveMaze>,
+    mut ev_generate_maze: EventReader<GenerateMazeEvent>,
+) {
+    let active_maze = active_maze.0;
+
+    for _ in ev_generate_maze.iter() {
+        let tile_storage = storage_query.get(active_maze).unwrap();
+        let grid_size = tile_storage.size;
+        println!("grid_sizes: {:?}", grid_size);
+        for x in 0..grid_size.x {
+            for y in 0..grid_size.y {
+                let tile_pos = TilePos { x, y };
+                commands
+                    .entity(tile_storage.get(&tile_pos).unwrap())
+                    .insert(TileColor(Color::BLACK))
+                    .insert(TileType::Floor);
+            }
+        }
+
+        // Recursive backtracking maze generation algorithm.
+        // https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
+        let mut rng = rand::thread_rng();
+        let current = TilePos { x: rng.gen_range(0..grid_size.x), y: rng.gen_range(0..grid_size.y) };
+        let mut stack = vec![current];
+        let mut visited = vec![current];
+        
+
+        // for x in 0..grid_size.x {
+        //     for y in 0..grid_size.y {
+        //         let is_floor: bool = rand::random();
+        //         // Grabbing neighbors is easy.
+        //         if is_floor {
+        //             commands
+        //                 .entity(tile_storage.get(&TilePos { x, y }).unwrap())
+        //                 .insert(TileColor(Color::WHITE))
+        //                 .insert(TileType::Floor);
+        //         }
+        //     }
+        // }
+    }
+}
+
+fn regenerate_on_click(
+    maze_query: Query<(&TilemapSize, &TilemapGridSize, &TilemapType, &Transform), With<Maze>>,
+    mouse_button: Res<Input<MouseButton>>,
+    cursor_pos: Res<CursorPos>,
+    active_maze: Res<ActiveMaze>,
+    mut ev_generate_maze: EventWriter<GenerateMazeEvent>,
+) {
+    if mouse_button.just_pressed(MouseButton::Left) {
+        let cursor_pos = cursor_pos.0;
+        let (map_size, grid_size, map_type, map_transform) = maze_query.get(active_maze.0).unwrap();
+        let cursor_in_map_pos: Vec2 = {
+            // Extend the cursor_pos vec3 by 0.0 and 1.0
+            let cursor_pos = Vec4::from((cursor_pos, 0.0, 1.0));
+            let cursor_in_map_pos = map_transform.compute_matrix().inverse() * cursor_pos;
+            cursor_in_map_pos.xy()
+        };
+        println!("cursor_pos: {:?}", cursor_pos);
+        if let Some(tile_pos) =
+            TilePos::from_world_pos(&cursor_in_map_pos, map_size, grid_size, map_type)
+        {
+            println!("tile_pos: {:?}", tile_pos);
+        }
+        ev_generate_maze.send(GenerateMazeEvent {});
+    }
+}
+
 fn text_update_system(
     diagnostics: Res<DiagnosticsStore>,
     cursor_pos: Res<CursorPos>,
     mut fps_query: Query<&mut Text, With<FpsText>>,
-    mut cursor_query: Query<&mut Text, (With<CursorPosText>, Without<FpsText>)>
+    mut cursor_query: Query<&mut Text, (With<CursorPosText>, Without<FpsText>)>,
 ) {
     for mut text in &mut fps_query {
         if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
@@ -238,4 +325,3 @@ pub fn update_cursor_pos(
         }
     }
 }
-
